860fbb82ce366ca3626a1b84a93874a9e9ee746d
Subham Khadka <subhamkhadka99@gmail.com>
2025-06-06 21:56:00 +0545

    Initial commit

---
+++ src/sections/Experiences.jsx
@@ -0,0 +1,13 @@
+import { Timeline } from "../components/Timeline";
+import { experiences } from "../constants";
+const Experiences = () => 
+      <section id ="work">
+
+  return 
+    <div className="w-full">
+      <Timeline data={experiences} />
+    </div>
+  
+</section>
+
+export default Experiences;
